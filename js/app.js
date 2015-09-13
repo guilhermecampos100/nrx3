d40bd00d08e792be82ed7f212be45bbd3bf9e3ae
Guilherme Campos <guilhermecampos100@gmail.com>
2015-09-13 15:31:41 -0300

    1.78

--- js/app.js
+++ js/app.js
@@ -462,7 +462,7 @@ app.factory('AboutData', function()
 		
 	
 		// APAGA FOTO
-		$scope.tirafoto = function(numfoto) {
+		$scope.apagafoto = function(numfoto) {
 			
 		var	nomefoto1 = $scope.secaoPai.codigo + "_foto_1.jpg";
 		var	nomefoto2 = $scope.secaoPai.codigo + "_foto_2.jpg";
