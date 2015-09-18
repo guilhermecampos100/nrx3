4881b1d1708b0f8de45b674e1965a3e8a2b0cd84
Guilherme Campos <guilhermecampos100@gmail.com>
2015-09-18 11:31:54 -0300

    2.216

--- js/app.js
+++ js/app.js
@@ -662,7 +662,7 @@ app.factory('AboutData', function()
 			db.transaction(function(tx) {
 				tx.executeSql('CREATE TABLE IF NOT EXISTS checklist_secoes (token text, codigo text, descricao text, secaopai text)');
 				tx.executeSql("Select count(*) as quantos from checklist_secoes where token=?", [$scope.token], function(tx, results) {
-					$scope.contaregistros = results.rows.item(0).quantos
+					$scope.contaregistros = results.rows.item(0).quantos;
 					$scope.$apply;
 				}
 				);
