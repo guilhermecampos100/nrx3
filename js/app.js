1fe807fe874f7d5ffe2e0580cc2fd34ec55d8970
Guilherme Campos <guilhermecampos100@gmail.com>
2015-09-18 08:25:46 -0300

    2.19

--- js/app.js
+++ js/app.js
@@ -657,10 +657,11 @@ app.factory('AboutData', function()
 			});
 			db.transaction(function(tx) {
 				for (var i=0; i < checklist_secoes.length; i++) {
-					tx.executeSql("INSERT INTO checklist_secoes (token, codigo, descricao, secaopai) VALUES (?,?,?,?)", [$rootScope.tokenGlobal, checklist_secoes[0].codigo, checklist_secoes[0].descricao, checklist_secoes[0].secaopai], function(tx, res) {
-						console.log("insertId: " + res.insertId + " -- " + checklist_secoes[0].codigo);
+					tx.executeSql("INSERT INTO checklist_secoes (token, codigo, descricao, secaopai) VALUES (?,?,?,?)", [$rootScope.tokenGlobal, checklist_secoes[i].codigo, checklist_secoes[i].descricao, checklist_secoes[i].secaopai], function(tx, res) {
+						console.log("insertId: " + res.insertId + " -- " + checklist_secoes[i].codigo);
 						console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
 						$scope.conta_atualizando = i + 1;
+						$scope.$apply();
 						//alert(res.insertId);
 					});
 				}
